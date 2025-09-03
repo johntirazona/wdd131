0d4b401e68f07661451d7a18d4379abb93e5abc9
johntirazona <johnjenlytirazona@gmail.com>
2025-09-03 12:06:54 +0800

    added javascript

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentYearSpan = document.getElementById("currentyear");
+
+const today = new Date();
+
+currentYearSpan.textContent = today.getFullYear();
+
+const lastModifiedParagraph = document.getElementById("lastModified");
+
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
