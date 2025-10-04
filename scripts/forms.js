7a32ce28f7fcf10ff81d68ea956435f05701aa82
johntirazona <johnjenlytirazona@gmail.com>
2025-10-04 18:43:50 +0800

    changed js

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,12 +1,31 @@
-// Product array
 const products = [
-  { id: "p1", name: "Laptop" },
-  { id: "p2", name: "Smartphone" },
-  { id: "p3", name: "Headphones" },
-  { id: "p4", name: "Tablet" }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
-// Populate select field
 document.addEventListener("DOMContentLoaded", () => {
   const select = document.getElementById("productName");
   products.forEach(product => {
